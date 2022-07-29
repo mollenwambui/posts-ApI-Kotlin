f1f39e1166a882113dd9d607dee7553af5d80ee8
Your Name <mollenwambui@gmail.com>
2022-07-29 17:18:46 +0300

    first commit

---
+++ app/src/main/java/com/example/mypost/PostRVAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.mypost
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mypost.databinding.PostListItemsBinding
+
+class PostRVAdapter (var context: Context, var postList: List<Post>):
+    RecyclerView.Adapter<PostRVAdapter.RetrofitViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RetrofitViewHolder {
+        var binding = PostListItemsBinding
+
+            .inflate(LayoutInflater.from(context), parent, false)
+        return RetrofitViewHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: RetrofitViewHolder, position: Int) {
+        var currentPost = postList.get(position)
+        with(holder.binding) {
+            tvUserId.text = currentPost.userId.toString()
+            tvId.text = currentPost.id.toString()
+            tvTitle.text = currentPost.title
+            tvBody.text = currentPost.body
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+
+
+    class RetrofitViewHolder(var binding: PostListItemsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
